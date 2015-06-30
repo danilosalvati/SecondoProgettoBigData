52e7878abc3546fb8c136d9d08bb6b038701cbd9
Danilo Salvati <sadan91@gmail.com>
2015-06-30 13:45:34 +0200

    Uploaded right version of top250 mapper

--- MapReduce/src/ActorsAndActresses250TopMovies/ActorsAndActresses250TopMoviesMapper.java
+++ MapReduce/src/ActorsAndActresses250TopMovies/ActorsAndActresses250TopMoviesMapper.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -20,17 +23,16 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class ActorsAndActresses250TopMoviesMapper extends
 		Mapper<Object, Text, Text, IntWritable> {
 
-	private static final IntWritable one = new IntWritable(1);
+	private List<String> top250movies = null;
 
-	private static List<String> top250movies = null;
-
-	protected void setup(Context context) throws IOException,
-			InterruptedException {
+	protected void setup(Context context) throws IOException {
 
 		if (top250movies == null) {
 
+			top250movies = new ArrayList<String>();
+
 			// Path del file in HDFS
-			Path path = new Path("hdfs://input/top250movies.list");
+			Path path = new Path("/input/top250movies.list");
 			FileSystem fs = FileSystem.get(new Configuration());
 			BufferedReader br = new BufferedReader(new InputStreamReader(
 					fs.open(path)));
@@ -41,11 +43,7 @@ public class ActorsAndActresses250TopMoviesMapper extends
 				top250movies.add(values[3]);
 				line = br.readLine();
 			}
-
 		}
-
-		// top250movies.add("boh"); //se decommenti questa riga va ma tira fuori
-		// dati sbagliati
 
 	}
 
@@ -55,10 +53,15 @@ public class ActorsAndActresses250TopMoviesMapper extends
 		/* Per prima cosa divido l'input in maniera opportuna */
 
 		String[] values = value.toString().split("\t");
-		boolean isInTop250 = false;
+		/*
+		 * Il Set è necessario per evitare di scrivere più volte lo stesso film
+		 * (dovuto a matching multipli della stessa sottostringa)
+		 */
 
-		if (values[1].toString().contains("<ENDVALUE>")) {
-			String[] movies = values[1].toString().split("<ENDVALUE>");
+		Set<String> moviesFound = new HashSet<String>();
+
+		if (values[1].contains("<ENDVALUE>")) {
+			String[] movies = values[1].split("<ENDVALUE>");
 
 			for (String movie : movies) {
 				/*
@@ -67,38 +70,33 @@ public class ActorsAndActresses250TopMoviesMapper extends
 				 */
 
 				/* Controllo se il film è nei top 250 */
-				//isInTop250 = false;
-
-				for (String topMovie : top250movies) {
-					if (movie.startsWith(topMovie)) {
-						isInTop250 = true;
+				boolean found = false;
+				int listSize = top250movies.size();
+				for (int i = 0; i < listSize && !found; i++) {
+					if (movie.startsWith(top250movies.get(i))) {
+						moviesFound.add(top250movies.get(i));
+						found = true;
 					}
-				}
-
-				if (isInTop250) {
-					context.write(new Text(values[0].toString()), one);
 				}
 			}
 		} else {
-			/* Controllo se il film è nei top 250 */
-			isInTop250 = false;
 
-			for (String topMovie : top250movies) {
-				if (values[1].toString().startsWith(topMovie)) {
-					isInTop250 = true;
+			/* Controllo se il film è nei top 250 */
+
+			boolean found = false;
+			int listSize = top250movies.size();
+			for (int i = 0; i < listSize && !found; i++) {
+				if (values[1].startsWith(top250movies.get(i))) {
+					moviesFound.add(top250movies.get(i));
+					found = true;
 				}
 			}
 
-			if (isInTop250) {
-				context.write(new Text(values[0].toString()), one);
-			}
+		}
+		if (moviesFound.size() != 0) {
+			context.write(new Text(values[0]),
+					new IntWritable(moviesFound.size()));
 		}
 	}
 
-	protected void cleanup(Context context) throws IOException,
-			InterruptedException {
-
-		top250movies.clear();
-
-	}
 }
